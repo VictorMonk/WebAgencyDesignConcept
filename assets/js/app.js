4aa5f889927b374a74a80a7d78d0611ab3a3abca
VictorMonk <107959125+VictorMonk@users.noreply.github.com>
2022-08-03 23:10:03 -0400

    Single Page Finished
    Home Page Finalized

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,28 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
+  
 });
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
